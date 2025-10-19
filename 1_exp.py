8295909e27ab8969455cee1b2c561131ca7d2ab2
jeyawani2009-ux <jeyawani2009@gmail.com>
2025-10-19 19:12:21 +0400

    Increase header font size from 8em to 12em

--- 1_exp.py
+++ 1_exp.py
@@ -44,7 +44,7 @@ html, body, [class*="css"] {
 h1 {
     font-family: 'Freestyle Script', cursive !important; /* handwritten style for header */
     text-align: center;
-    font-size: 8em;
+    font-size: 12em;
     color: #6D2E46;
     margin-top: 2em;
 }
