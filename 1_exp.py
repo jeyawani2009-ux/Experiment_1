447910496d399487baed7e6dc661cef5e0059654
jeyawani2009-ux <jeyawani2009@gmail.com>
2025-10-19 19:16:24 +0400

    Reduce font size of h1 header from 12em to 8em

--- 1_exp.py
+++ 1_exp.py
@@ -44,7 +44,7 @@ html, body, [class*="css"] {
 h1 {
     font-family: 'Freestyle Script', cursive !important; /* handwritten style for header */
     text-align: center;
-    font-size: 12em !important;
+    font-size: 8em !important;
     color: #6D2E46;
     margin-top: 2em;
 }
