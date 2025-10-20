1b9f1100fb5fab875d75419da9fcf32c8713f377
jeyawani2009-ux <jeyawani2009@gmail.com>
2025-10-20 12:48:11 +0400

    Update 1_exp.py

--- 1_exp.py
+++ 1_exp.py
@@ -29,6 +29,69 @@ page_bg = """
 """
 st.markdown(page_bg, unsafe_allow_html=True)
 
+st.markdown("""
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Freestyle+Script:wght@300;500&family=Playfair+Display:wght@600&display=swap');
+
+html, body, [class*="css"] {
+    background-color: #F8F7F3; /* darker aesthetic beige */
+    background-attachment: fixed;
+    background-size: cover;
+    color: #3B2A4A;
+    font-family: 'Poppins', sans-serif;
+}
+
+/* Remove Streamlit's default padding */
+[data-testid="stAppViewContainer"] {
+    padding-top: 0rem !important;
+    padding-left: 2rem !important;
+    margin-top: 0rem !important;
+}
+
+/* Handwritten header animation */
+h1 {
+    font-family: 'Freestyle Script', cursive !important;
+    text-align: left;
+    font-size: 8em !important;
+    color: #6D2E46;
+    margin-top: -5rem !important;
+    margin-bottom: 0.1em !important;
+}
+p, h2 {
+    text-align: left
+    font-size: 1.1em;
+}
+
+.stButton>button {
+    background-color: #B088F9;
+    color: white;
+    border: none;
+    padding: 0.8em 2em;
+    border-radius: 30px;
+    font-size: 1.2em;
+    transition: 0.3s ease-in-out;
+    display: block;
+    margin: 2em auto;
+}
+
+.stButton>button:hover {
+    background-color: #A16AE8;
+    transform: scale(1.05);
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}0
+
+div, h1, h2, p {
+  animation: fadeIn 1.5s ease-in-out;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown(page_bg, unsafe_allow_html=True)
+
 # -------------------- NAVIGATION --------------------
 if "page" not in st.session_state:
     st.session_state.page = "home"
